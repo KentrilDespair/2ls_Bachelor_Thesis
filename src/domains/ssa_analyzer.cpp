7373d59b57d1bdf98e0b09bc25e42ef81539b238
KentrilDespair <kentril.despair@gmail.com>
2019-03-05 04:01:44 +0100

    Added debugging outputs, implemented find_goto_instrs() method, inmprecision invariant identification now works for heap domain

--- src/domains/ssa_analyzer.cpp
+++ src/domains/ssa_analyzer.cpp
@@ -6,9 +6,7 @@ Author: Peter Schrammel
 
 \*******************************************************************/
 
-#ifdef DEBUG
 #include <iostream>
-#endif
 
 #include <solvers/sat/satcheck.h>
 #include <solvers/flattening/bv_pointers.h>
@@ -44,6 +42,12 @@ Author: Peter Schrammel
   *static_cast<tpolyhedra_domaint *>(domain), solver, SSA, SSA.ns)
 #endif
 
+#define ODebug(fmt) do {								\
+	std::cerr << __FILE__ << ":" << __LINE__ << ":" << 	\
+	__FUNCTION__ << "():" << fmt << "\n"; } while(0)	
+
+#define debug() (std::cerr)
+
 /*******************************************************************\
 
 Function: ssa_analyzert::operator()
@@ -74,6 +78,7 @@ void ssa_analyzert::operator()(
   // add precondition (or conjunction of asssertion in backward analysis)
   solver << precondition;
 
+  ODebug("DOmain: ");
   domain=template_generator.domain();
 
   // get strategy solver from options
@@ -155,7 +160,7 @@ void ssa_analyzert::operator()(
   }
   else
   {
-    if(template_generator.options.get_bool_option("enum-solver"))
+    if(template_generator.options.get_bool_option("enum-solver"))//> TODO HERE
     {
       s_solver=new strategy_solvert(
         *static_cast<tpolyhedra_domaint *>(domain),
@@ -177,8 +182,9 @@ void ssa_analyzert::operator()(
         template_generator);
       result=new predabs_domaint::templ_valuet();
     }
-    else if(template_generator.options.get_bool_option("binsearch-solver"))
+    else if(template_generator.options.get_bool_option("binsearch-solver"))	//> TODO HERE
     {
+		ODebug("Binsearch-solver new templ_valuet");
       result=new tpolyhedra_domaint::templ_valuet();
       s_solver=new BINSEARCH_SOLVER;
     }
@@ -189,7 +195,11 @@ void ssa_analyzert::operator()(
   s_solver->set_message_handler(get_message_handler());
 
   // initialize inv
+  ODebug("Solver INITIALIZE domain");
   domain->initialize(*result);
+
+  ODebug("Solver - OUTPUTTING SSA");
+  SSA.output(std::cout);
 
   // iterate
   while(s_solver->iterate(*result)) {}
@@ -201,9 +211,16 @@ void ssa_analyzert::operator()(
   solver_calls+=s_solver->get_number_of_solver_calls();
   solver_instances+=s_solver->get_number_of_solver_instances();
 
-  // TODO
-  // imprecise invariant identification method call for supported domains
-  domain->identify_invariant_imprecision(*result, SSA.ns);
+  // TODO TODO TODO ---------------------------------------------------
+
+  // getting ssa vars identifs and locations
+  std::vector<std::pair<unsigned, std::string>> ssa_vars_locs;
+
+  domain->identify_invariant_imprecision(*result, ssa_vars_locs);
+  // pass only necessary stuff and not big TODO
+  find_goto_instrs(SSA, ssa_vars_locs);
+
+  // ------------------------------------------------------------------
 
   delete s_solver;
 }
@@ -222,6 +239,8 @@ Function: ssa_analyzert::get_result
 
 void ssa_analyzert::get_result(exprt &_result, const domaint::var_sett &vars)
 {
+	ODebug("Getting result... HERE");
+  // TODO
   domain->project_on_vars(*result, vars, _result);
 }
 
@@ -258,4 +277,93 @@ Function: ssa_analyzert::input_heap_bindings
 const exprt ssa_analyzert::input_heap_bindings()
 {
   return static_cast<heap_domaint &>(*domain).get_iterator_bindings();
+}
+
+/*******************************************************************\
+
+Function: ssa_analyzert::find_goto_instrs
+
+  Inputs: TODO
+
+ Outputs: TODO
+
+ Purpose: TODO
+
+\*******************************************************************/
+void ssa_analyzert::find_goto_instrs(
+  local_SSAt &SSA, 
+  std::vector<std::pair<unsigned, std::string>> &ssa_vars_locs)
+{
+  // for each variable at location in SSA
+  for (auto it=ssa_vars_locs.begin(); it!=ssa_vars_locs.end(); it++)
+  {
+    // get SSA node on that location - end of the loop for loop back var
+    local_SSAt::nodest::iterator a_it=SSA.find_node(
+      SSA.get_location(it->first));
+
+    // statement on location of loop back jump
+    // debug() << "Statement: " 
+    //  << from_expr(SSA.ns, "", *(a_it->equalities.begin())) << "\n";
+
+    
+    // get start of the loop node for that node
+    local_SSAt::nodest::iterator loop_it=a_it->loophead;
+    // node with the last assignment
+    local_SSAt::nodest::iterator last_it; 
+
+    // starting after the loophead, should never be end TODO
+    loop_it++;  
+
+    // looping through each node in the loop
+    while (loop_it!=a_it)
+    {
+      // TODO equalities only
+      for (local_SSAt::nodet::equalitiest::const_iterator e_it=
+          loop_it->equalities.begin(); e_it!=loop_it->equalities.end();
+          e_it++)
+      {
+        // debug() << "(LOOP) " << from_expr(SSA.ns,"",e_it->lhs()) << "\n";
+
+        // get the SSA variable identifier on the left-hand side
+        std::string var_lhs=from_expr(SSA.ns, "", e_it->lhs());
+
+        // skip any guards, conditions, ... starting with '$'
+        if (var_lhs[0]=='$')
+          continue;
+
+        // strip of '#'
+        size_t idx=var_lhs.find('#');
+        if(idx!=std::string::npos)
+          var_lhs=var_lhs.substr(0, idx);
+
+        // compare with the var 
+        if (it->second==var_lhs)
+        {
+          debug() << "MATCH: " << from_expr(SSA.ns, "",e_it->lhs()) << "\n";
+          last_it=loop_it;
+        }
+
+        // instruction at that location
+        // debug() << "STAT: " << from_expr(SSA.ns, "", 
+        //  loop_it->location->code); //code.get_statement() << "\n";
+      }
+      loop_it++;
+    }
+
+    // type of instr: should be ASSIGN
+    // debug() << l_it->location->type << "\n";
+
+    // debug() << l_it->location->code.get_statement() << "\n";
+    // debug() << l_it->location->function << "\n";
+
+    // SSA loc: 
+    // debug() << l_it->location->location_number << "\n";
+
+    debug() << "\n-> Variable \"" << it->second << "\" in ";
+    debug() << last_it->location->source_location.as_string() << "\n\n";
+
+    // debug() << l_it->location->source_location.get_line() << "\n";
+    // debug() << l_it->location->source_location.get_comment() << "\n";
+    // debug() << l_it->location->source_location.get_function() << "\n";
+  }
 }
